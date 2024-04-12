ac830a8fabb8107115cbb779c3f99ecf52eceeda
jorsal26 <salinasjorgeluis26@gmail.com>
2024-04-11 21:55:03 -0300

    Primer Commit de la Pre-entrega 2

---
+++ codigo.js
@@ -0,0 +1,179 @@
+
+var pedidos = [];
+var clienteEncontrado = [];
+var productoEncontrado = [];
+var nroPedido = 0;
+var totalPedido = 0.00;
+//fecha
+const fechaDeHoy = new Date();
+
+function generarPresupuesto(){
+console.log('CLIENTES');
+console.table(clientes);
+
+let menu1 = parseInt(prompt('Ingresa Opcion Deseada\n1-Filtrar Clientes por Nombre\n2-Buscar Cliente\n3-Mostrar Clientes Con Pedidos\n0-PARA SALIR'));
+
+while (menu1 != 0) {
+    switch (menu1) {
+        case 1:
+            //filtrado
+            let opcion = prompt('Ingrese Nombre Cliente a Filtrar: ');
+            filtrarClientes(opcion);
+            break;
+        case 2:
+            //buscar por nombre de cliente
+            let encotreCliente = 0;
+            let nombre = prompt('Ingresa el Nombre del Cliente a Buscar');
+            if (nombre != '') {
+                encotreCliente=buscarCliente(nombre)
+                if (encotreCliente>0){
+                    let menu2 = parseInt(prompt('Cliente Seleccionado\n\nID:'+clienteEncontrado.id+'\nNOMBRE:'+clienteEncontrado.name+'\n\n1-Crear Pedido\n0-PARA SALIR'));
+                    while (menu2 != 0) {
+                        switch (menu2) {
+                            case 1:
+                                // console.log('PRODUCTOS');
+                                // console.table(productos);
+                                crearPedido(encotreCliente);
+                                break;
+                            default:
+                                break;
+                        }
+                        menu2 = parseInt(prompt('Cliente Seleccionado\n\nID:'+clienteEncontrado.id+'\nNOMBRE:'+clienteEncontrado.name+'\n\n1-Crear Pedido\n0-PARA SALIR'));
+                    }
+                }
+            } else {
+                alert('No ingreso ningun Nombre')
+            }
+            break;
+        case 3:
+            //funcion para contar pedidos realizados
+            clientesConPedidos();
+            break;
+        default:
+            alert('Opcion Incorrecta!');
+            break;
+    }
+
+    menu1 = parseInt(prompt('Ingresa Opcion Deseada\n1-Filtrar Clientes por Nombre\n2-Buscar Cliente\n3-Mostrar Clientes Con Pedidos\n0-PARA SALIR'));
+}
+}
+
+
+function filtrarClientes(nombre) {
+    let filtroCliente = [];
+
+    filtroCliente = clientes.filter(clientes => clientes.name.toLowerCase().includes(nombre.toLowerCase()));
+    if(filtroCliente.length > 0){
+        console.log('CLIENTES Encotrados con: '+nombre);
+        console.table(filtroCliente);
+    }else{
+        console.log('Ningun Nombre de Cliente Coincide con el dato Ingresado!')
+    }
+}
+
+
+function buscarCliente(nombre) {
+    clienteEncontrado = clientes.find(clientes => clientes.name.toLowerCase().includes(nombre.toLowerCase()));
+
+    if (clienteEncontrado != undefined) {
+        alert(`Cliente encontrado:\nID: ${clienteEncontrado.id} \nNOMBRE: ${clienteEncontrado.name} \nEMAIL: ${clienteEncontrado.email} \nPEDIDO: ${clienteEncontrado.numeroDePedido} \nTOTAL PEDIDO: $ ${clienteEncontrado.totalPedido} \nFECHA PEDIDO: ${clienteEncontrado.fechaPedido}`);
+        return clienteEncontrado.id
+    } else {
+        console.log('Cliente no encontrado');
+        return 0
+    }
+}
+
+function buscarPedido(pedido) {
+    let pedidoEncontrado = pedidos.filter(pedidos => pedidos.id == pedido.id);
+
+    if (pedidoEncontrado != undefined) {
+        console.log('Datos del Pedido: '+pedido.id);
+        console.table(pedidoEncontrado);
+    } else {
+        console.log('Pedido no encontrado');
+    }
+}
+
+function buscarProducto(idProducto) {
+    productoEncontrado = productos.find(productos => productos.id===idProducto);
+
+    if (productoEncontrado != undefined) {
+        alert(`Producto encontrado:\nID:${productoEncontrado.id} \nDESCRIPCION:${productoEncontrado.nombre} \nPRECIO: $${productoEncontrado.precio}`);
+        return productoEncontrado.id
+    } else {
+        console.log('Producto no encontrado');
+        return 0
+    }
+}
+
+function clientesConPedidos() {
+    let filtroClientesPedidos = [];
+
+    filtroClientesPedidos = clientes.filter(clientes => clientes.numeroDePedido > 0);
+    if(filtroClientesPedidos.length > 0){
+        console.log('CLIENTES CON PEDIDOS');
+        console.table(filtroClientesPedidos);
+    }else{
+        console.log('Ningun Cliente tiene Pedidos!')
+    }
+}
+
+function crearPedido(idCliente) {
+    pedidos = [];
+    totalPedido = 0.00;
+    
+    let menu1 = parseInt(prompt('Cliente Seleccionado\n\nID: '+clienteEncontrado.id+'\nNOMBRE: '+clienteEncontrado.name+'\n\n1-Buscar Producto\n2-Confirmar Pedido y SALIR\n0-PARA SALIR'));
+    while (menu1 != 0) {
+        switch (menu1) {
+            case 1:
+                //buscar por id de producto
+                console.log('PRODUCTOS');
+                console.table(productos);
+
+                let idProducto = parseInt(prompt('Ingresa el ID de Producto a Seleccionar'));
+                if (idProducto != 0) {
+                    encotreProducto=buscarProducto(idProducto)
+                    if (encotreProducto>0){
+                        let cantidadProducto = parseInt(prompt('Ingresa Cantidad del Producto Seleccionado'));
+                        if (cantidadProducto != 0) {
+                            pedidos.push({idPedido: nroPedido,idCliente: clienteEncontrado.id,idProducto: productoEncontrado.id,nombreProducto: productoEncontrado.nombre,precioProducto: productoEncontrado.precio,cantidadProducto: cantidadProducto,totalProductos: productoEncontrado.precio*cantidadProducto,fechaPedido: fechaDeHoy});
+                            console.log('PEDIDO Sin Confirmar');
+                            console.table(pedidos);
+
+                            // acumular segun una columna con reduce
+                            totalPedido = pedidos.reduce((prev, curr) => prev + curr.totalProductos, 0);
+
+                            console.log('TOTAL PEDIDO: $ '+totalPedido);
+                        } else {
+                            alert('No ingreso Cantiad del Producto')
+                        }
+                    }
+                } else {
+                    alert('No ingreso ningun Id de Producto')
+                }
+                break;
+            case 2:
+                //Confirmar Pedido y Salir
+                if (pedidos.length > 0) {
+                    let indice = clientes.findIndex(index => index.id == clienteEncontrado.id);
+                    if (indice >= 0) {
+                        nroPedido += 1;
+                        clientes[indice].numeroDePedido = nroPedido;
+                        clientes[indice].totalPedido = totalPedido;
+                    } else {
+                        alert('No se pudo obtener el idice del cliente a actualizar.')
+                    }
+                } else {
+                    alert('Pedido no Generado.')
+                }
+                break;
+            default:
+                break;
+        }
+        menu1 = parseInt(prompt('Cliente Seleccionado\n\nID: '+clienteEncontrado.id+'\nNOMBRE: '+clienteEncontrado.name+'\n\n1-Buscar Producto\n2-Confirmar Pedido y SALIR\n0-PARA SALIR'));
+    }
+}
+
+
+
---
+++ productos.js
@@ -0,0 +1,79 @@
+const productos = [{
+    id: 1,
+    foto: "",
+    nombre: "Masitas surtidas por Kilo.",
+    precio: 10000
+},
+{
+    id: 2,
+    foto: "",
+    nombre: "Facturas por docena.",
+    precio: 5000
+},
+{
+    id: 3,
+    foto: "",
+    nombre: "Tarta de Coco.",
+    precio: 6500
+},
+{
+    id: 4,
+    foto: "",
+    nombre: "Tarta de Frutilla.",
+    precio: 7000
+},
+{
+    id: 5,
+    foto: "",
+    nombre: "Tarta Cabsha",
+    precio: 8500
+},
+{
+    id: 6,
+    foto: "",
+    nombre: "Tarta Pirineos.",
+    precio: 9000
+},
+{
+    id: 7,
+    foto: "",
+    nombre: "Tarta de Durazonos.",
+    precio: 7000
+},
+{
+    id: 8,
+    foto: "",
+    nombre: "Tortitas.",
+    precio: 120
+},
+{
+    id: 9,
+    foto: "",
+    nombre: "Criollitos por Kilo.",
+    precio: 5000
+},
+{
+    id: 10,
+    foto: "",
+    nombre: "Criollitos por Kilo.",
+    precio: 5000
+},
+{
+    id: 11,
+    foto: "",
+    nombre: "Torta decorada con crema por Kilo.",
+    precio: 6000
+},
+{
+    id: 12,
+    foto: "",
+    nombre: "Torta Polonesa Frutilla/Durazno por Kilo.",
+    precio: 7000
+},
+{
+    id: 13,
+    foto: "",
+    nombre: "Torta Selva Negra por Kilo.",
+    precio: 8000
+}
+];
